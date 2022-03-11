f8ada8251738fd5a156ca46381a9f15125d524e1
cly <597450096@qq.com>
2022-03-11 14:53:23 +0800

    init

---
+++ mall-member/src/test/java/com/cly/gumall/member/MallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cly.gumall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
